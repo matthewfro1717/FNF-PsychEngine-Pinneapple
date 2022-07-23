7d5c40c0955181d5cf8c78e303944198ae0397c6
InsKal <94728303+InsKal@users.noreply.github.com>
2022-07-23 17:20:10 -0400

    yes

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -116,7 +116,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(!closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/jigsaw-4277821/FNF-PsychEngine-Android-Support/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/InsKal/FNF-PsychEngine-With-Android-Support/main/gitVersion.txt");
 			http.onData = function (data:String)
 			{
 				updateVersion = data.split('\n')[0].trim();
